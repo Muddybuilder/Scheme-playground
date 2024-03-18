711afa0683a4dea3bb99f392b14536b122a158cf
Hoon Jeong <yong08211@gmail.com>
2024-03-17 22:28:07 -0400

    recursive vs iterative process

---
+++ recursive_process.rkt
@@ -0,0 +1,22 @@
+; Linear iterative Process
+; Expansion (build up of chain of deffered operations) followed by contraction.
+(define (factorial n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)
+      )
+  )
+
+; Linear recursive process
+; The process does not grow or shink as it evolves.
+(define (fact2 n)
+  (if (< n 1)
+      1
+      (* n (fact2 (- n 1)))))
+
+(fact2 40)
+  
